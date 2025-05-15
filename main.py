869f6fef9d1017979aa4d64e4447a061d82af156
Prerana S <prerana.1si22is070@gmail.com>
2025-05-15 15:28:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from prediction_helper import predict
 
-st.title("Health Insurance Premium Prediction Appli")
+st.title("Health Insurance Premium Prediction App")
 
 st.header("Input Details")
 
